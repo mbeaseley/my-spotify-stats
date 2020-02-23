c0d70145a0dede01cab1d0c90ec6cb552516114d
mbeaseley <m.t.beaseley@gmail.com>
2020-02-23 18:31:32 +0000

    setup of modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import(`./admin/admin.module`)
+      .then(m => m.AdminModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
