060ddeeb6e4374c8bac2da4766e6368edf7e41cd
mbeaseley <m.t.beaseley@gmail.com>
2020-02-23 18:45:40 +0000

    login page

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
 
+import { LoginComponent } from './components/login/login.component';
 
+const routes: Routes = [
+  { path: '', component: LoginComponent},
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
 })
 export class AdminModule { }
